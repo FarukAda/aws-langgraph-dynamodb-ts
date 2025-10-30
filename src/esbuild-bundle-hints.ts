46407dd527af2c3b833b48ddcc7e8e4ea8e8f4a8
FarukAda <faruk88ada@gmail.com>
2025-10-30 19:55:33 +0100

    test: change esbuild hits

--- src/esbuild-bundle-hints.ts
+++ src/esbuild-bundle-hints.ts
@@ -1,14 +1,6 @@
-try {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  import('jsonpath/include/module.js');
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  import('jsonpath/include/action.js');
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  import('esprima');
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-} catch (e) {
-  // Ignore if not present
-}
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('jsonpath/include/module.js');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('jsonpath/include/action.js');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('esprima');
