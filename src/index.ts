5c5d272a5d9fdeb0acadc020f877dd695ba58cbb
FarukAda <faruk88ada@gmail.com>
2025-10-30 19:49:26 +0100

    test: add esbuild hints

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import './esbuild-bundle-hints';
 export { DynamoDBSaver } from './checkpointer';
 export type { DynamoDBSaverOptions } from './checkpointer/types';
 export { DynamoDBStore } from './store';
